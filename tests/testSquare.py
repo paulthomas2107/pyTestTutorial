57d867e6a0039f2aedfba459e3fc378d3b254d63
Paul Thomas <paulthomas2107@gmail.com>
2023-10-23 19:07:01 +0100

    More

--- tests/testSquare.py
+++ tests/testSquare.py
@@ -2,6 +2,12 @@ import pytest
 import source.shapes as shapes
 
 
-@pytest.mark.parametrize("side_length, expected_area", [(5, 25), (4, 16), (9, 81), (5, 25)])
-def test_multiple_square_areas(side_length, expected_area):
+@pytest.mark.parametrize("side_length, expected_area, random", [(5, 25, 12), (4, 16, 78), (9, 81, 987), (5, 25, 112)])
+def test_multiple_square_areas(side_length, expected_area, random):
+    assert random > side_length
     assert shapes.Square(side_length).area() == expected_area
+
+
+@pytest.mark.parametrize("side_length, expected_perimeter", [(3, 12), (4, 16), (5, 20)])
+def test_multiple_perimeters(side_length, expected_perimeter):
+    assert shapes.Square(side_length).perimeter() == expected_perimeter
