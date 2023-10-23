6c89bff7c1c01fb931c5a30f9c32f6314e30e4cd
Paul Thomas <paulthomas2107@gmail.com>
2023-10-23 16:57:11 +0100

    More

--- source/myFunctions.py
+++ source/myFunctions.py
@@ -3,4 +3,6 @@ def add(num1: int, num2: int) -> int:
 
 
 def divide(num1: float, num2: float) -> float:
+    if num2 == 0:
+        raise ValueError
     return num1 / num2
--- tests/testMyFunctions.py
+++ tests/testMyFunctions.py
@@ -5,3 +5,18 @@ import source.myFunctions as myFunctions
 def test_add():
     result = myFunctions.add(1, 4)
     assert result == 5
+
+
+def test_add_strings():
+    result = myFunctions.add("I like ", "Burgers")
+    assert result == "I like Burgers"
+
+
+def test_divide():
+    result = myFunctions.divide(10, 5)
+    assert result == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        myFunctions.divide(10, 0)
